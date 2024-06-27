8cebf818ec76556d31871cebd7c06e7aa68f2c70
pankaj <0xpankaj@gmail.com>
2024-06-28 00:59:41 +0545

    signin

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,19 +1,22 @@
 import AuthHeading from "./AuthHeading";
 import AuthLayout from "./AuthLayout";
+import Button from "./Buttom";
+import ButtonWarming from "./ButtonWarming";
+import InputBox from "./InputBox";
+import SubHeading from "./SubHeading";
 
 export default function Signin() {
     return (
         <AuthLayout>
             <AuthHeading label={"SignIn"} />
-         <div>
-         <div>
-                <input type="text" placeholder="Email" />
-                
+            <SubHeading label={"signin form for paytm"} />
+            <InputBox label={"Email"} placeholder={"john@example.com"} />
+            <InputBox label={"Password"} placeholder={"password"} />
+            <div >
+                <Button label={"SignIn"} />
             </div>
-            <div>
-                <input type="text" placeholder="Password" />
-            </div>
-         </div>
+            <ButtonWarming label={"Don't have an account?"} buttonText={"SignUp"}  />
+         
         </AuthLayout>
         
     )
